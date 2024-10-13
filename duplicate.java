f6079254fbf310cc817f1eb27c6487026490bc55
Achint Verma <achintberma@gmail.com>
2024-10-13 09:33:52 +0530

    Create duplicate.java

---
+++ duplicate.java
@@ -0,0 +1,22 @@
+Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+Example 1:
+Input: nums = [1,2,3,1]
+Output: true
+Explanation:
+The element 1 occurs at the indices 0 and 3.
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> h = new HashSet<>();
+        for(int i=0; i<nums.length; i++){
+            if(!h.contains(nums[i])){
+                h.add(nums[i]);
+            }
+            else{
+                return true;
+            }
+        }
+        return false;
+    }
+}
